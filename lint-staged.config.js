756a6e3b50b4d0cb156579d1f676a7a43337807c
swfz <sawafuji.09@gmail.com>
2022-06-12 17:46:48 +0900

    fix: secretlint エラーで落とすのを差分に対して引っかかる項目がある時に絞った

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -4,6 +4,6 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-  '*': (filenames) => `${__dirname}/node_modules/.bin/secretlint --secretlintrc ${__dirname}/.secretlintrc.json ${filenames.join(' ')}`,
+  '*': (filenames) => `${__dirname}/node_modules/.bin/secretlint --format checkstyle --secretlintrc ${__dirname}/.secretlintrc.json ${filenames.join(' ')} | reviewdog -fail-on-error -f checkstyle -diff "git diff --cached" -reporter local`,
   '.github/workflows/*.{yml,yaml}': (filenames) => `actionlint ${filenames.join(' ')}`
 }
