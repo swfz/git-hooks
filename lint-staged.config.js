2245994a2ed8dea974a54359f7b2e562cb0521f8
swfz <sawafuji.09@gmail.com>
2022-06-13 15:20:22 +0900

    feat: actionlint diff --cached only

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,5 +5,5 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   '*': (filenames) => `${__dirname}/node_modules/.bin/secretlint --format checkstyle --secretlintrc ${__dirname}/.secretlintrc.json ${filenames.join(' ')} | reviewdog -fail-on-error -f checkstyle -diff "git diff --cached" -reporter local`,
-  '.github/workflows/*.{yml,yaml}': (filenames) => `actionlint ${filenames.join(' ')}`
+  '.github/workflows/*.{yml,yaml}': (filenames) => `actionlint -format '{{range $err := .}}{{$err.Filepath}}:{{$err.Line}}:{{$err.Column}} {{$err.Kind}} {{$err.Message}}\n{{$err.Snippet}}\n\n{{end}}' ${filenames.join(' ')} | reviewdog -fail-on-error -efm="%f:%l:%c %m" -diff='git diff --cached' -reporter local`
 }
