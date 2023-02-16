c2b011949ef2e84f03726d9ec43df6ad21a2514d
swfz <sawafuji.09@gmail.com>
2023-02-17 01:45:27 +0900

    feat: console.logが紛れていないかチェックする

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -5,5 +5,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
   '*': (filenames) => `${__dirname}/node_modules/.bin/secretlint --format checkstyle --secretlintrc ${__dirname}/.secretlintrc.json ${filenames.join(' ')} | reviewdog -fail-on-error -f checkstyle -diff "git diff --cached" -reporter local`,
-  '.github/workflows/*.{yml,yaml}': (filenames) => `actionlint -format '{{range $err := .}}{{$err.Filepath}}:{{$err.Line}}:{{$err.Column}} {{$err.Kind}} {{$err.Message}}\n{{$err.Snippet}}\n\n{{end}}' ${filenames.join(' ')} | reviewdog -fail-on-error -efm="%f:%l:%c %m" -diff='git diff --cached' -reporter local`
+  '.github/workflows/*.{yml,yaml}': (filenames) => `actionlint -format '{{range $err := .}}{{$err.Filepath}}:{{$err.Line}}:{{$err.Column}} {{$err.Kind}} {{$err.Message}}\n{{$err.Snippet}}\n\n{{end}}' ${filenames.join(' ')} | reviewdog -fail-on-error -efm="%f:%l:%c %m" -diff='git diff --cached' -reporter local`,
+  '*.@(js|ts|tsx)': (filenames) => `grep -nH console.log ${filenames.join(' ')} | reviewdog -fail-on-error -efm="%f:%l: %m" -diff='git diff --cached' -reporter local`,
 }
